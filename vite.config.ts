7ba8d310a49e8a8f29aec27846ff7e64cb4fa041
Md. Jakaria Islam Rakin <128883910+RakinJakaria@users.noreply.github.com>
2025-08-20 04:21:28 +0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Quran-Ayah-AI-Canvas/",   // 👈 Add this line for GitHub Pages
+  base: "/",
   server: {
     host: "::",
     port: 8080,
